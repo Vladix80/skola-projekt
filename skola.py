7640a1118a7e873f4aec61244dd78b9f72cc4e8d
Vladix80 <carcharodon11@gmail.com>
2026-06-03 15:38:47 +0200

    prvy projekt Python + SQL

---
+++ skola.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import pyodbc
+
+conn = pyodbc.connect(
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=(localdb)\\MSSQLLocalDB;"
+    "DATABASE=Škola;"
+    "Trusted_Connection=yes;"
+)
+
+dotaz = "SELECT * FROM dbo.Študenti"
+df = pd.read_sql(dotaz, conn)
+
+pd.set_option('display.max_columns', None)
+print(df.head())
