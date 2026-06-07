fbaf400bc1e9f79cc03bef5ac108de198ca586a3
Vladix80 <carcharodon11@gmail.com>
2026-06-07 13:37:12 +0200

    vymazane cvicne subory

--- kontrola1.py
+++
@@ -1,7 +0,0 @@
-pocet = 0
-
-while pocet <3:
-    print(f"pokus cislo {pocet}")
-    pocet = pocet + 1
-   
-    print("Koniec")
--- uloha1.py
+++
@@ -1,6 +0,0 @@
-print("=== KALKULAČKA SPREPITNÉHO ===")
-print("")
-
-ucet = input("Kolko bol účet?")
-percenta_sprepitneho = input("Kolko percent sprepitného?")
-Kolko_ludí = input ("Kolko vás bolo?")
